1ae5c5f82f62f55f03d115abbde77ff08fa2c689
Jorge Loza Olvera <jorge.loza5670@alumnos.udg.mx>
2026-04-04 22:20:47 -0600

    Versión_1: ragas evaluation. Se definió la función de evaluación en rag_eval.py y modificó la funcuión de ejecución del RAG en main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+"""
+Modificaciones: 
+-Eliminaicón del ciclo while para interacción con usuario.
+-Implememtación de nueva función ejecutar_rag (revisar abajo)
+"""
+
+
 from langchain_ollama.llms import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
 from vector import retriever
@@ -5,7 +12,8 @@ from vector import retriever
 model = OllamaLLM(model="llama3.2")
 
 template = """
-You are "Jhon Tv Show" an expert in answering questions about entretainment focusing on TV and movies
+You are "Jhon Tv Show" an expert in answering questions about entretainment focusing on TV and movies.
+Your task is to answer only using the provide context. If the answer is not in the given context, say: "I'm sorry. I have no information to answet that."
 
 Here are some relevant shows rankings: {shows_rankings}
 
@@ -15,6 +23,7 @@ Here is the question to answer: {question}
 prompt = ChatPromptTemplate.from_template(template)
 chain = prompt | model
 
+"""
 while True:
     print("\n\n---------------------------------------")
     question = input("Ask your question (q to quit): ")
@@ -25,4 +34,39 @@ while True:
     shows_rankings = retriever.invoke(question)
     result = chain.invoke({"shows_rankings":shows_rankings, "question": question})
     print(result)
+"""
 
+
+#Función final que es útil para ragas.
+#NOTA: RECIBE DESCRIPCIÓN, TÍTULO Y METADATA. 
+#Quité el while xd
+
+def ejecutar_rag(question):
+    docs = retriever.invoke(question)
+
+    contexts = [
+    f"""
+     Title: {doc.metadata['title']}
+     Metascore: {doc.metadata['metascore']}
+     Userscore: {doc.metadata['userscore']}
+     Date: {doc.metadata['date']}
+
+    Description:
+    {doc.page_content}
+    """
+    for doc in docs
+]
+    contexts_text = "\n\n".join(contexts)
+
+    result = chain.invoke({
+        "shows_rankings": contexts_text,
+        "question" : question
+    })
+    
+    answer = result.content
+
+    return {
+        "question": question,
+        "answer": answer,
+        "contexts": contexts
+    }
--- rag_eval.py
+++ rag_eval.py
@@ -1,44 +1,70 @@
 """
-Este programa ejecuta las evaluaciones de faithfulness y answer_relevancy usando la librería ragas. 
-Necesita:
-- Ejecutar las preguntas (question) en el sistema RAG
-- Colocar las salidas del RAG en orden, tomando solo el context de la función modificada en el archivo main.py
+En este programa se llevan a cabo las evaluaciones del sistema RAG.
+Para la primera prueba, se establecieron 15 preguntas para verificar (no tuve oportunidad de ejecutar pq no tengo ni GPU ni internet)
+So this code may explode. Be careful. 
+Al ejecutarse, el programa: 
+- Definirá las listas con preguntas y verdades estructuradas medinate un proceso mixto (manual, LLM Local) sobre la base de datos metric.tv_shows.csv
+- Mediante un ciclo for, llamará a la función "ejecutar_rag" del main.py.
+- Recuperará las repsuestas y contextos del RAG, questions and ground_truths y formará un dataset from Dataset.
+- Con el Dataset formado, se pasará a evaluación con ragas, usando faithfulness y asnwer_relevancy. 
+- Solo Dios sabe que pasará cuando se ejecute.
 """
 
 import ragas
 from datasets import Dataset
 from ragas.metrics.collections import faithfulness, answer_relevancy
 from ragas import evaluate
+from main import ejecutar_rag
 
-evaluation_data_faith ={
-    "question": [
+questions = [
         "¿De qué trata The Last of Us?",
         "¿De qué genero es The last of Us?",
         "¿Cuántas temporadas tiene la serie de Cosmos: A space-time Odyssey?",
         "¿En qué está basado el documental de Cosmos?",
         "¿En qué está basada Arcane?",
-        "¿De qué géneros es la serie Arcane?"
+        "¿De qué géneros es la serie Arcane?",
+        "¿Cuál es la puntiación de The Wire?",
+        "¿Quién dirigió Bo Burnham: Inside?",
+        "¿Quiénes son los directores de The Vietnam War?",
+        "¿Cuál es la puntiación de The Last Dance?",
+        "¿Cúal es el puntaje de Bleak House?",
+        "¿Quién dirigío The Wire Season 2?",
+        "¿A qué género pertence The Americans?"
+        "Dame el usersocre del documental que habla sobre Michael Jordan"
+        "¿Cuál es el nombre de la serie ambientada en personajes de League of Legends?"
 
+]
 
-    ],
-    "context": [ #Faltaría agregar las respuestas que arroje el RAG 
-
-    ],
-    "answer": [ #Faltaría agregar las respuestas que arroge el RAG
-
-    ],
-
-    "ground_truth":[
+ground_truths = [
         "Set 20 years after the destruction of civilization, Joel (Pedro Pascal) is hired to smuggle 14-year-old Ellie (Bella Ramsey) out of a quarantine zone in this drama series based on the PlayStation video game of the same name.",
         "Action,Adventure,Drama,Horror,Sci-Fi,Thriller",
         "1 temporada.",
         "Based on the Carl Sagan's original Cosmos series, astrophysicist Neil deGrasse Tyson hosts this new version.",
         "The animated series based on the League of Legends follows the origin of two League champions in the prosperous city of Piltover and the underground district of Zaun.",
-        "Animation,Action,Adventure,Drama,Fantasy,Sci-Fi"
-    ]
-}
+        "Animation,Action,Adventure,Drama,Fantasy,Sci-Fi",
+        "92",
+        "Bo Burnham",
+        "Ken Burns, Lynn Novick",
+        "88",
+        "78",
+        "No disponible en la base de datos.",
+        "No disponible en la base de datos.",
+        "The Last Dance, 88.",
+        "Arcane"
 
-dataset_faith = Dataset.from_dict(evaluation_data_faith)
-print(dataset_faith)
+]
 
-results = evaluate(dataset=dataset_faith, metrics=[faithfulness])
+dataset = []
+
+for q,g_t in zip(questions, ground_truths):
+    result = ejecutar_rag(q)
+
+    dataset.append({
+        "question": q,
+        "answer": result["answer"],
+        "contexts": result["contexts"],
+        "ground_truth": g_t
+    })
+
+dataset_evaluation = Dataset.from_list(dataset)
+results = evaluate(dataset=dataset_evaluation, metrics=[faithfulness, answer_relevancy])
